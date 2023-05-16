a33a43548a60060900b6b1f341a3aa16c24c3cf2
Rigan Gor <rigan.gor@gmail.com>
2023-05-16 10:24:58 +0300

    keycloak start server script

--- start-keycloak.sh
+++ start-keycloak.sh
@@ -14,7 +14,7 @@ fi
 
 # Copy certificates to the current directory
 echo "Copying certificates from '$CERT_DIR' to the current directory..."
-cp -R "$CERT_DIR" .
+sudo cp -R "$CERT_DIR" .
 ls -a .
 
 # Start the Docker Compose setup
